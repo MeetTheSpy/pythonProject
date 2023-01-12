30157b7615c7cf5599e03df811f46bce5846fadf
user <12345671321home>
2023-01-12 10:25:02 +0300

    32131312

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-
+import os
+import sys
 
 class Board:
     def __init__(self, width, height):
@@ -14,6 +15,21 @@ class Board:
         self.left = left
         self.top = top
         self.cell_size = cell_size
+
+    def load_image(name, colorkey=None):
+        fullname = os.path.join('data', name)
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
 
     def render(self, screen):
         colors = ['black', 'red', 'yellow', 'blue', 'green', 'purple', 'pink']
